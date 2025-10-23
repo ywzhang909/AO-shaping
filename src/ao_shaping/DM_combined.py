feat(tuning): integrate visualization and centroid calculation into dm unit compute refactor(dm): update driver and utility functions for better initialization fix(camera): handle invalid image data in daheng driver perf(wfs): improve zernike coefficient retrieval and testing chore(utils): add rms and energy based voltage initialization utilities test(driver): update driver test to use rms-based initialization

src/ao_shaping/DM_combined.py
@@ -7,6 +7,7 @@ import tqdm
 import pygame
 
 from ao_shaping.drivers import MlaRes, NlightDM, Thorlab_WFS, CameraStreamManager
+from ao_shaping.utils import get_init_V_by_rms
 
 
 ROOT_DIR = "./data/wf"
@@ -90,7 +91,8 @@ def render(window,
     window.blit(pos_img_canvas, (0,0))
     # 扩大wavefront使得大小和img一样
     disp_wf = np.resize(wavefront[0], (img_x, img_y))
-    
+    # fill nan in disp_wf
+    disp_wf[np.isnan(disp_wf)] = 0
     neg_img_canvas = pygame.surfarray.make_surface(disp_wf.transpose())
     pygame.draw.circle(neg_img_canvas, (255, 0, 0), center, r, 1)
     window.blit(neg_img_canvas, (img_y+5,0))
@@ -121,7 +123,7 @@ def optimizer(
     epochs = int(epochs)
     _init_v = np.array(init_v)
     with Thorlab_WFS(MlaRes.Res768, use_custom_ref=False, high_speed=True, pupil_diameter=Pupil_Diameter) as wfs,\
-            CameraStreamManager(cam_id=0, exposure_time_ms=CAM_EXP_TIME, skip_sampling=False) as cam,\
+            CameraStreamManager(cam_id=1, exposure_time_ms=CAM_EXP_TIME, skip_sampling=False) as cam,\
             NlightDM(keep_when_exit=KEEP_VOLTAGE_WHEN_EXIT) as dm:
         assert len(init_v) == dm.DM_Num
         dm.send_voltages(_init_v, 0.5)
@@ -169,7 +171,7 @@ def optimizer(
                 "_img": img[np.newaxis, ...],
             }
         ]
-        mode = 'rms' if rms > rms_threshold else 'pib'
+        mode = 'rms' if 6 > rms > rms_threshold else 'pib'
         
         if show:
             pygame.init()
@@ -194,7 +196,7 @@ def optimizer(
                 neg_img, neg_pib = calc_pib(bucket_mask)
 
                 # 如果rms大于阈值, 使用rms作用目标函数；否则使用pib
-                if min(pos_rms, neg_rms) > rms_threshold:
+                if min(pos_rms, neg_rms) > rms_threshold and max(pos_pib, neg_pib) < 6.0:
                     diff = -(pos_rms-neg_rms)
                     mode = 'rms'
                 else:
@@ -254,7 +256,7 @@ def run():
     init_V = np.zeros((64,))
     wfs_history = optimizer(
         init_v=init_V.tolist(),
-        epochs=10000)
+        epochs=100_000)
 
     # Find best voltage
     id_max = np.argmax([h["J"] for h in wfs_history])

src/ao_shaping/drivers/ccd/daheng.py
@@ -136,10 +136,10 @@ class CameraStreamManager:
         for _ in range(n_sample):
             while True:
                 raw_image = self.cam.data_stream[0].get_image()
-                if not raw_image:
+                if any([not raw_image, raw_image.get_numpy_array() is None]):
                     continue
                 
-                numpy_image += raw_image.get_numpy_array()
+                numpy_image += raw_image.get_numpy_array().astype(np.uint8)
                 break
         avg_img = numpy_image/n_sample
         return avg_img.astype(np.uint8)

src/ao_shaping/drivers/dm/NLight.py
@@ -198,17 +198,23 @@ class DMSdk:
             raise Exception("device connection error.")
 
 
-def test():
-    import os
-    import tqdm
-    
-    v_dump_path = os.path.join(os.path.dirname(__file__), 'to_load_V.csv')
-    # v = np.loadtxt(v_dump_path)
-    with NLight(keep_when_exit=True) as dm:
-        for i in tqdm.trange(100_000):
-            v = np.zeros((dm.DM_Num,))
-            v[1] = 30 * np.sin(2* np.pi * (i/10))
-            dm.send_voltages(v, 0.2)
+
 
 if __name__ == '__main__':
-    test()
+    def test():
+        import os
+        import tqdm
+        
+        v_dump_path = os.path.join(os.path.dirname(__file__), 'to_load_V.csv')
+        # v = np.loadtxt(v_dump_path)
+        with NLight(keep_when_exit=True) as dm:
+            for i in tqdm.trange(100_000):
+                v = np.zeros((dm.DM_Num,))
+                v[1] = 30 * np.sin(2* np.pi * (i/10))
+                dm.send_voltages(v, 0.2)
+                
+    def turn_off():
+        with NLight(keep_when_exit=False) as dm:
+            dm.reset_all()
+            
+    turn_off()

src/ao_shaping/drivers/wfs/thorlab_wfs.py
@@ -489,7 +489,8 @@ class WFSManager:
         # wavefront = np.where(wavefront==np.nan, 0, wavefront)
         return wavefront, {"min":min.value, "max":max.value, "diff":diff.value, "mean":mean.value, "rms":rms.value, "wighted_rms":wighted_rms.value}
 
-    def get_zernike(self, zernike_order=5, image_loop_counter: int = -1):
+    def get_zernike(self, zernike_order=10, image_loop_counter: int = -1):
+        assert zernike_order <= 10, "zernike order must be less than or equal to 10"
         roc_mm = c_double()
         coeff_num =  (zernike_order + 1) * (zernike_order + 2) // 2 + 1
         zernike_order = c_int32(zernike_order)
@@ -683,7 +684,23 @@ if __name__ == '__main__':
                 print(y[0,:])
                 # print(wfs.get_zernike(3))
                 print(wfs.get_wavefront()[0][0,:])
+                
+    def test_rms():
+        rms_hist = []
+        with WFSManager(MlaRes.Res768) as wfs:
+            wfs.high_speed = True
+            for _ in range(100):
+                wfs.take_image(n_sample=10)
+                
+                # dx, dy = wfs.get_spot_deviation()
+                # rms = np.sqrt(np.nanmean(dx**2+dy**2))
+                zernike_coeff = wfs.get_zernike(10)
+                print(zernike_coeff)
+                rms_hist.append(np.mean(np.sqrt(np.sum(zernike_coeff**2))))
+        return rms_hist
+    # test_wfs()
 
-    test_wfs()
-
+    rms_hist = test_rms()
+    plt.plot(rms_hist)
+    plt.show()
     

src/ao_shaping/wf/DM_wfs.py
@@ -8,6 +8,7 @@ import pandas as pd
 
 import tqdm
 from ao_shaping.drivers import MlaRes, NlightDM, Thorlab_WFS
+from ao_shaping.utils import get_init_V_by_rms
 
 ROOT_DIR = "./data/wf"
 
@@ -90,8 +91,8 @@ def save_list(dir, data):
 
 def optimizer(
     epochs,
-    delta=1,
-    lr=1,
+    delta=1.0,
+    lr=1.0,
     weight_decay=0.001,
     algorithm: Literal[
         "spgd", "adam", "nadam", "adamod", "cool_momentum_spgd"
@@ -219,10 +220,16 @@ def optimizer(
                     lr = 1.2
                 elif avg_j > 0.15:
                     delta = 1
-                    lr = 1.0
-                else:
+                    lr = 1.1
+                elif avg_j > 0.12:
+                    delta = 0.9
+                    lr = 0.9
+                elif avg_j > 0.08:
                     delta = 0.8
                     lr = 0.8
+                else:
+                    delta = 0.7
+                    lr = 0.7
                 
                 log = {
                     "J": avg_j,
@@ -243,7 +250,7 @@ def optimizer(
         return history
 
 def run():
-    init_V = np.loadtxt("data/flatten_voltages/20251016/rms-0.130.csv")
+    init_V = get_init_V_by_rms()
     res_list = optimizer(
         init_v=init_V.copy(),
         epochs=10000, 

test/driver_test.py
@@ -45,6 +45,7 @@ def test_all(vs: np.ndarray):
         
         
 if __name__ == "__main__":
-    # vs = np.loadtxt("data/to_load_V-9635.0.csv")
-    # test_all(vs)
-    turn_off_dm()
+    from ao_shaping.utils import get_init_V_by_rms
+    vs = get_init_V_by_rms()
+    test_all(vs)
+    # turn_off_dm()